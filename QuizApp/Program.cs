6ccda952124579e51872b9de506165582ef90049
Alex <zhukaleksazhuk@gmail.com>
2024-03-09 22:24:01 +0300

    save tests result in separate json file

--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Newtonsoft.Json;
+using QuizApp.Questions;
+using QuizApp.Results;
 
 namespace QuizApp
 {
@@ -6,10 +8,14 @@ namespace QuizApp
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Hallo, what is your name?");
+			string userName = Console.ReadLine();
 			string json = File.ReadAllText("C:\\Users\\Alexa\\Desktop\\test\\q.json");
 			AllQuestions questionsData = JsonConvert.DeserializeObject<AllQuestions>(json);
 
 			int rightAnswersCount = 0;
+			List<AnswerDetail> userAnswers = new List<AnswerDetail>();
+
 			foreach (QuestionDescription description in questionsData.Questions)
 			{
 				int answerNumber = 1;
@@ -25,13 +31,35 @@ namespace QuizApp
 					Console.WriteLine("Try again");
 				}
 
+				AnswerDetail detail = new AnswerDetail();
+				detail.Question = description.Question;
 				if (userAnswer == description.RightIndex + 1)
 				{
 					rightAnswersCount += 1;
+					detail.IsRight = true;
 				}
+				else
+				{
+					detail.IsRight = false;
+				}
+				userAnswers.Add(detail);
 			}
 
 			Console.WriteLine($"You have {rightAnswersCount} right answers from {questionsData.Questions.Count} \n");
+
+			TestResult testResult = new TestResult();
+			testResult.WrongAnswers = questionsData.Questions.Count - rightAnswersCount;
+			testResult.RightAnswers = rightAnswersCount;
+			testResult.Details = userAnswers;
+
+			string path = $"C:\\Users\\Alexa\\Desktop\\test\\{userName}_{DateTime.Now:dd-MM-yyyy_hh-mm-ss}.json";
+			string resultTry = JsonConvert.SerializeObject(testResult);
+			File.WriteAllText(path, resultTry);
+
+
+
 		}
+
+
 	}
 }
---
+++ QuizApp/Results/TestResult.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizApp.Results
+{
+	internal class TestResult
+	{
+		public int RightAnswers { get; set; }
+		public int WrongAnswers { get; set; }
+
+		public List<AnswerDetail> Details { get; set; }
+
+	}
+}
